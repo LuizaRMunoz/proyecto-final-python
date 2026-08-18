e5f16370647187108890e4db32bee19819084083
Luiza Gama Muñoz <luizarmunoz@gmail.com>
2026-08-18 15:57:35 -0300

    Instalado Python, archivo requirements.txt creado y carpeta del proyecto empempezada

---
+++ gestor_activos_incidentes/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gestor_activos_incidentes project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestor_activos_incidentes.settings')
+
+application = get_asgi_application()
